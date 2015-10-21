ada5b547590f9ffcb41358b02e80a0f1a0881c0a
Rost Siryk <rostislav.siryk+github@gmail.com>
2015-10-21 23:41:50 +0300

    node express app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+/* globals require, console */
+
+'use strict';
+
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res) {
+	// res.render('home', {
+	//   title: 'Welcome'
+	// });
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+var server = app.listen(3000, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at http://%s:%s', host, port);
+});
